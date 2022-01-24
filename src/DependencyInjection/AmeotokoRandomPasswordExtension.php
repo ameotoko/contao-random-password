4396a0d328854112093f4cd9edda29ab8725848e
ameotoko <andrey.vinichenko@gmail.com>
2022-01-24 21:48:19 +0100

    rename argument according to parent method signature

--- src/DependencyInjection/AmeotokoRandomPasswordExtension.php
+++ src/DependencyInjection/AmeotokoRandomPasswordExtension.php
@@ -15,7 +15,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class AmeotokoRandomPasswordExtension extends Extension
 {
-    public function load(array $mergedConfig, ContainerBuilder $container): void
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
             $container,
